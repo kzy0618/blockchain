dc3d7421f180db3de62996908aeee55ecfef56ca
kzy618 <zkon584@aucklanduni.ac.nz>
2018-09-09 16:17:55 +1200

    add vote function

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -18,12 +18,17 @@ contract Election{
 		uint voteCount;
 	}
 
+	//Store accounts that have voted
+
+	mapping(address => bool) public voters;
 	//Store Candidate
 	//Fetch Candidate
 
 	//any key does not set value will return blank as default
 	//no way to get the size of the map -> uses candidatesCount
 	mapping(uint => Candidate) public candidates;
+
+
 	//Store Candidate Constuctor
 	uint public candidatesCount;
 
@@ -37,5 +42,24 @@ contract Election{
 		candidatesCount ++;
 		candidates[candidatesCount] = Candidate(candidatesCount,_name,0);
 	}
+
+	//create transaction -> if it does not pass the condition only the gas spent on the require part are costed others will be refunded
+	function vote (uint _candidateId) public{
+		//reuquire that they haven't voted before
+		//accept condition -> if true execute rest of the codes, other wise stop
+		require(!voters[msg.sender]);
+
+		//require a valid candidate
+		require(_candidateId > 0 && _candidateId <= candidatesCount);
+
+		//record that voter has voted
+		//sender -> account from which this message sent
+		//app.vote(1,{from: web3.eth.accounts[0]}) -> from parameter is where solidity read as msg.sender
+		voters[msg.sender] = true;
+
+		//update candidate vote Count
+		candidates[_candidateId].voteCount ++;
+	}
+
 
 }
--- src/js/app.js
+++ src/js/app.js
@@ -70,12 +70,34 @@ App = {
           var candidateTemplate = "<tr><th>" + id + "</th><td>" + name + "</td><td>" + voteCount + "</td></tr>"
           candidatesResults.append(candidateTemplate);
 
+          // Render candidate ballot option
+          var candidateOption = "<option value='" + id + "' >" + name + "</ option>"
+          candidatesSelect.append(candidateOption);
         });
+      }
+      return electionInstance.voters(App.account);
+    }).then(function(hasVoted) {
+      // Do not allow a user to vote
+      if(hasVoted) {
+        $('form').hide();
       }
       loader.hide();
       content.show();
     }).catch(function(error) {
       console.warn(error);
+    });
+  },
+
+  castVote: function() {
+    var candidateId = $('#candidatesSelect').val();
+    App.contracts.Election.deployed().then(function(instance) {
+      return instance.vote(candidateId, { from: App.account });
+    }).then(function(result) {
+      // Wait for votes to update
+      $("#content").hide();
+      $("#loader").show();
+    }).catch(function(err) {
+      console.error(err);
     });
   }
 };
